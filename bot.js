bd773f537ad08dfededaa3421643568454fed053
ASDANDROID <52899527+ASDANDROID@users.noreply.github.com>
2019-09-13 17:20:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', message => {
 } 
 
 
-if (message.content.startsWith(prefix + 'stname')) {
+if (message.content.startsWith(prefix + 'setname')) {
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`Username Changed To **${argresult}**`)
   return message.reply("You Can change the username 2 times per hour");
